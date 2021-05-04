ea611508e63c074929468536e60a20fee5c5ce88
Hansel Tritama <10084360+hanselgunawan@users.noreply.github.com>
2021-05-03 20:32:28 -0700

    added onclick logic

--- app/src/main/java/com/hanseltritama/tictactoegame/MainActivity.kt
+++ app/src/main/java/com/hanseltritama/tictactoegame/MainActivity.kt
@@ -2,10 +2,53 @@ package com.hanseltritama.tictactoegame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.GridLayout
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var o_turn: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupUI()
+    }
+
+    private fun setupUI() {
+        for (imageId in 0..8) {
+            val scale: Float = applicationContext.resources.displayMetrics.density
+            val imageView = ImageView(this)
+            val param: GridLayout.LayoutParams = GridLayout.LayoutParams()
+            Glide.with(this)
+                .load(R.drawable.ic_launcher_background)
+                .into(imageView)
+            param.height = (100 * scale + 0.5f).toInt()
+            param.width = (100 * scale + 0.5f).toInt()
+            param.setMargins(5,5,5,5)
+            imageView.id = imageId
+            imageView.layoutParams = param
+            imageView.setOnClickListener {
+                onImageClick(imageId)
+            }
+            grid_layout_container.addView(imageView)
+        }
+    }
+
+    private fun onImageClick(imageIndex: Int) {
+        val clickedImage: ImageView = findViewById(imageIndex)
+        if (!o_turn) {
+            Glide.with(this)
+                .load(R.drawable.ximage)
+                .into(clickedImage)
+        } else {
+            Glide.with(this)
+                .load(R.drawable.oimage)
+                .into(clickedImage)
+        }
+        o_turn = !o_turn
     }
 }
